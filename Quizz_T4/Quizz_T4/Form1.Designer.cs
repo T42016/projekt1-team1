74d97268e065073e7b153cb6f2c164883b4143e8
RasmusT4 <rasmus.bjorklund@lund.ntig.se>
2016-08-26 10:41:41 +0200

    gränssnitt 2.0

--- Quizz_T4/Quizz_T4/Form1.Designer.cs
+++ Quizz_T4/Quizz_T4/Form1.Designer.cs
@@ -31,8 +31,12 @@
             this.components = new System.ComponentModel.Container();
             this.tc = new System.Windows.Forms.TabControl();
             this.tabMenu = new System.Windows.Forms.TabPage();
+            this.btnQuizChoose = new System.Windows.Forms.Button();
+            this.btnResults = new System.Windows.Forms.Button();
+            this.btnQuizCreator = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
             this.tabQuiz = new System.Windows.Forms.TabPage();
+            this.prgBar30s = new System.Windows.Forms.ProgressBar();
             this.rtbxQuestion = new System.Windows.Forms.RichTextBox();
             this.btnAnsr4 = new System.Windows.Forms.Button();
             this.btnAnsr3 = new System.Windows.Forms.Button();
@@ -52,7 +56,12 @@
             this.tbxAnswer1 = new System.Windows.Forms.TextBox();
             this.lblQuestion = new System.Windows.Forms.Label();
             this.timer30s = new System.Windows.Forms.Timer(this.components);
-            this.prgBar30s = new System.Windows.Forms.ProgressBar();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lboxQuestions = new System.Windows.Forms.ListBox();
+            this.lboxQuiz = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.tc.SuspendLayout();
             this.tabMenu.SuspendLayout();
             this.tabQuiz.SuspendLayout();
@@ -68,27 +77,57 @@
             this.tc.Margin = new System.Windows.Forms.Padding(2);
             this.tc.Name = "tc";
             this.tc.SelectedIndex = 0;
-            this.tc.Size = new System.Drawing.Size(250, 432);
+            this.tc.Size = new System.Drawing.Size(500, 432);
             this.tc.TabIndex = 0;
             // 
             // tabMenu
             // 
+            this.tabMenu.Controls.Add(this.btnQuizChoose);
+            this.tabMenu.Controls.Add(this.btnResults);
+            this.tabMenu.Controls.Add(this.btnQuizCreator);
             this.tabMenu.Controls.Add(this.btnStart);
             this.tabMenu.Location = new System.Drawing.Point(4, 22);
             this.tabMenu.Margin = new System.Windows.Forms.Padding(2);
             this.tabMenu.Name = "tabMenu";
             this.tabMenu.Padding = new System.Windows.Forms.Padding(2);
-            this.tabMenu.Size = new System.Drawing.Size(242, 406);
+            this.tabMenu.Size = new System.Drawing.Size(492, 406);
             this.tabMenu.TabIndex = 0;
             this.tabMenu.Text = "Menu";
             this.tabMenu.UseVisualStyleBackColor = true;
             // 
+            // btnQuizChoose
+            // 
+            this.btnQuizChoose.Location = new System.Drawing.Point(227, 276);
+            this.btnQuizChoose.Name = "btnQuizChoose";
+            this.btnQuizChoose.Size = new System.Drawing.Size(129, 53);
+            this.btnQuizChoose.TabIndex = 3;
+            this.btnQuizChoose.Text = "Välj Quiz";
+            this.btnQuizChoose.UseVisualStyleBackColor = true;
+            // 
+            // btnResults
+            // 
+            this.btnResults.Location = new System.Drawing.Point(92, 276);
+            this.btnResults.Name = "btnResults";
+            this.btnResults.Size = new System.Drawing.Size(129, 53);
+            this.btnResults.TabIndex = 2;
+            this.btnResults.Text = "Tidigare resultat";
+            this.btnResults.UseVisualStyleBackColor = true;
+            // 
+            // btnQuizCreator
+            // 
+            this.btnQuizCreator.Location = new System.Drawing.Point(227, 214);
+            this.btnQuizCreator.Name = "btnQuizCreator";
+            this.btnQuizCreator.Size = new System.Drawing.Size(129, 53);
+            this.btnQuizCreator.TabIndex = 1;
+            this.btnQuizCreator.Text = "Quiz Creator";
+            this.btnQuizCreator.UseVisualStyleBackColor = true;
+            // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(5, 324);
+            this.btnStart.Location = new System.Drawing.Point(92, 214);
             this.btnStart.Margin = new System.Windows.Forms.Padding(2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(233, 42);
+            this.btnStart.Size = new System.Drawing.Size(129, 53);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Starta Quiz";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -96,6 +135,7 @@
             // 
             // tabQuiz
             // 
+            this.tabQuiz.Controls.Add(this.btnQuit);
             this.tabQuiz.Controls.Add(this.prgBar30s);
             this.tabQuiz.Controls.Add(this.rtbxQuestion);
             this.tabQuiz.Controls.Add(this.btnAnsr4);
@@ -106,14 +146,21 @@
             this.tabQuiz.Margin = new System.Windows.Forms.Padding(2);
             this.tabQuiz.Name = "tabQuiz";
             this.tabQuiz.Padding = new System.Windows.Forms.Padding(2);
-            this.tabQuiz.Size = new System.Drawing.Size(242, 406);
+            this.tabQuiz.Size = new System.Drawing.Size(492, 406);
             this.tabQuiz.TabIndex = 1;
             this.tabQuiz.Text = "Quiz";
             this.tabQuiz.UseVisualStyleBackColor = true;
             // 
+            // prgBar30s
+            // 
+            this.prgBar30s.Location = new System.Drawing.Point(116, 332);
+            this.prgBar30s.Name = "prgBar30s";
+            this.prgBar30s.Size = new System.Drawing.Size(237, 23);
+            this.prgBar30s.TabIndex = 5;
+            // 
             // rtbxQuestion
             // 
-            this.rtbxQuestion.Location = new System.Drawing.Point(6, 6);
+            this.rtbxQuestion.Location = new System.Drawing.Point(118, 9);
             this.rtbxQuestion.Margin = new System.Windows.Forms.Padding(2);
             this.rtbxQuestion.Name = "rtbxQuestion";
             this.rtbxQuestion.Size = new System.Drawing.Size(234, 106);
@@ -122,37 +169,37 @@
             // 
             // btnAnsr4
             // 
-            this.btnAnsr4.Location = new System.Drawing.Point(135, 218);
+            this.btnAnsr4.Location = new System.Drawing.Point(242, 217);
             this.btnAnsr4.Margin = new System.Windows.Forms.Padding(2);
             this.btnAnsr4.Name = "btnAnsr4";
-            this.btnAnsr4.Size = new System.Drawing.Size(105, 146);
+            this.btnAnsr4.Size = new System.Drawing.Size(111, 94);
             this.btnAnsr4.TabIndex = 3;
             this.btnAnsr4.Text = "4";
             this.btnAnsr4.UseVisualStyleBackColor = true;
             // 
             // btnAnsr3
             // 
-            this.btnAnsr3.Location = new System.Drawing.Point(6, 215);
+            this.btnAnsr3.Location = new System.Drawing.Point(116, 217);
             this.btnAnsr3.Margin = new System.Windows.Forms.Padding(2);
             this.btnAnsr3.Name = "btnAnsr3";
-            this.btnAnsr3.Size = new System.Drawing.Size(124, 151);
+            this.btnAnsr3.Size = new System.Drawing.Size(113, 94);
             this.btnAnsr3.TabIndex = 2;
             this.btnAnsr3.Text = "3";
             this.btnAnsr3.UseVisualStyleBackColor = true;
             // 
             // btnAnsr2
             // 
-            this.btnAnsr2.Location = new System.Drawing.Point(122, 116);
+            this.btnAnsr2.Location = new System.Drawing.Point(242, 119);
             this.btnAnsr2.Margin = new System.Windows.Forms.Padding(2);
             this.btnAnsr2.Name = "btnAnsr2";
-            this.btnAnsr2.Size = new System.Drawing.Size(120, 94);
+            this.btnAnsr2.Size = new System.Drawing.Size(110, 94);
             this.btnAnsr2.TabIndex = 1;
             this.btnAnsr2.Text = "2";
             this.btnAnsr2.UseVisualStyleBackColor = true;
             // 
             // btnAnsr1
             // 
-            this.btnAnsr1.Location = new System.Drawing.Point(4, 116);
+            this.btnAnsr1.Location = new System.Drawing.Point(116, 119);
             this.btnAnsr1.Margin = new System.Windows.Forms.Padding(2);
             this.btnAnsr1.Name = "btnAnsr1";
             this.btnAnsr1.Size = new System.Drawing.Size(113, 94);
@@ -162,6 +209,11 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.btnRemove);
+            this.tabPage1.Controls.Add(this.lboxQuiz);
+            this.tabPage1.Controls.Add(this.lboxQuestions);
             this.tabPage1.Controls.Add(this.rtbnQuestion);
             this.tabPage1.Controls.Add(this.btnAdd);
             this.tabPage1.Controls.Add(this.lblCorrectAnswer);
@@ -178,7 +230,7 @@
             this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
-            this.tabPage1.Size = new System.Drawing.Size(242, 406);
+            this.tabPage1.Size = new System.Drawing.Size(492, 406);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "Kia grejor";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -288,18 +340,63 @@
             this.timer30s.Interval = 1000;
             this.timer30s.Tick += new System.EventHandler(this.timer30s_Tick);
             // 
-            // prgBar30s
+            // btnQuit
             // 
-            this.prgBar30s.Location = new System.Drawing.Point(7, 371);
-            this.prgBar30s.Name = "prgBar30s";
-            this.prgBar30s.Size = new System.Drawing.Size(230, 23);
-            this.prgBar30s.TabIndex = 5;
+            this.btnQuit.Location = new System.Drawing.Point(7, 344);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(95, 53);
+            this.btnQuit.TabIndex = 6;
+            this.btnQuit.Text = "Avbryt Quiz";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // lboxQuestions
+            // 
+            this.lboxQuestions.FormattingEnabled = true;
+            this.lboxQuestions.Location = new System.Drawing.Point(238, 20);
+            this.lboxQuestions.Name = "lboxQuestions";
+            this.lboxQuestions.Size = new System.Drawing.Size(110, 238);
+            this.lboxQuestions.TabIndex = 13;
+            // 
+            // lboxQuiz
+            // 
+            this.lboxQuiz.FormattingEnabled = true;
+            this.lboxQuiz.Location = new System.Drawing.Point(365, 20);
+            this.lboxQuiz.Name = "lboxQuiz";
+            this.lboxQuiz.Size = new System.Drawing.Size(110, 238);
+            this.lboxQuiz.TabIndex = 14;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(377, 264);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 15;
+            this.btnRemove.Text = "Ta bort Quiz";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Ta bort Fråga";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Lägg till Quiz";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 435);
+            this.ClientSize = new System.Drawing.Size(512, 435);
             this.Controls.Add(this.tc);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
@@ -339,6 +436,15 @@
         private System.Windows.Forms.Label lblCorrectAnswer;
         private System.Windows.Forms.Timer timer30s;
         private System.Windows.Forms.ProgressBar prgBar30s;
+        private System.Windows.Forms.Button btnQuizChoose;
+        private System.Windows.Forms.Button btnResults;
+        private System.Windows.Forms.Button btnQuizCreator;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox lboxQuiz;
+        private System.Windows.Forms.ListBox lboxQuestions;
     }
 }
 
