551a8331609c1e7aa929d2d79962f7e736ef21db
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-02 09:41:01 +0200

    added emptying quiz creator text boxes when returning to main menu

--- Quizz_T4/Quizz_T4/Form1.cs
+++ Quizz_T4/Quizz_T4/Form1.cs
@@ -568,6 +568,12 @@ namespace Quizz_T4
             tabMenu.Enabled = true;
             tc.SelectedTab = tabMenu;
             rtbxListQuestions.Text = "";
+            tbxQuizzName.Text = "";
+            rtbnQuestion.Text = "";
+            tbxAnswer1.Text = "";
+            tbxAnswer2.Text = "";
+            tbxAnswer3.Text = "";
+            tbxAnswer4.Text = "";
         }
 
        
